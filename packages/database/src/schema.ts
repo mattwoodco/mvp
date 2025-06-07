d68f67ffa4bd13e6fc34d429f82b036f5f60b143
Matt Wood <matt@mattwood.co>
2025-06-07 15:52:36 -0600

    fix database schema

--- packages/database/src/schema.ts
+++
@@ -1,5 +0,0 @@
-import { auth } from "./schemas/auth.schema";
-import { listings } from "./schemas/listings.schema";
-import { users } from "./schemas/users.schema";
-
-export { auth, users, listings };
---
+++ packages/database/src/schema/index.ts
@@ -0,0 +1,3 @@
+export { account, session, verification } from "./auth.schema";
+export { listing } from "./listings.schema";
+export { user } from "./users.schema";
